fbefccc9176671276770a306f0a0240aee22a593
Vítor Fraporti Rosmann <110407685+yThanos@users.noreply.github.com>
2022-11-09 10:22:14 -0300

    Add files via upload

---
+++ exercicios/vetores/exerc6.cs
@@ -0,0 +1,25 @@
+﻿internal class program
+{
+    static void Main(String[] args)
+    {
+        int[] vetor = new int[10];
+        int[] vetor2 = new int[10];
+        int p = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Digite o valor da posição: " + i);
+            vetor[i] = int.Parse(Console.ReadLine());
+        }
+        for(int i = 9; i >= 0; i--)
+        {
+            vetor2[i] = vetor[p];
+            p++;
+        }
+        for(int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(vetor[i]);
+            Console.WriteLine(vetor2[i]);
+        }
+    }
+}
---
+++ exercicios/vetores/exerc7.cs
@@ -0,0 +1,35 @@
+﻿internal class program
+{
+    static void Main(String[] args)
+    {
+        int[] vetor = new int[10];
+        int[] vetor2 = new int[10];
+        int p = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Digite o valor da posição: " + i);
+            vetor[i] = int.Parse(Console.ReadLine());
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            if(vetor[i] % 2 == 0)
+            {
+                vetor2[p] = vetor[i];
+                p = p + 1;
+            }
+        }
+        for(int i = 0; i < 10; i++)
+        {
+            if (vetor[i] % 2 != 0)
+            {
+                vetor2[p] = vetor[i];
+                p = p + 1;
+            }
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(vetor2[i]);
+        }
+    }
+}
